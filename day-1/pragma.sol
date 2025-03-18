3e3e9526a4f0ed30e0ebdbd85b47d6d38e2842bb
cahyoromadhon <gantengcahyo27@gmail.com>
2025-03-18 21:47:24 +0700

    Introduce Pragma

---
+++ day-1/pragma.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+// singkatnya pragma berguna untuk mendeklarasikan versi dari compiler yang akan digunakan
+// versi 0.8.26 adalah versi paling baru saat ini per tanggal 18-3-2025
+// mengapa menggunakan '^' nahh ini adalah tanda bahwa versi kompiler tersebut dan yang lebih tinggi lagi dapat kompatibel dengan kode yang sedang ditulis saat ini
+// file prettirerc.json berguna untuk membuat syntaks agar lebih rapih dan sesuai dengan default penulisan solidity
+// git ini opsional dan bisa ada bisa tidak tergantung jika kode ini mau dipublikasikan atau tidak sesuai developer
+// setelah mengkompile sebuah smart contract biasanya folder artifact muncul yang menandakan bahwa proses kompilasi telah berhasil dan informasi telah disimpan dengan format .json
+// id dari hasil kompilasi data akan berubah seiring dengan proses kompilasi dan akan terus berubah mengikuti perubahan
